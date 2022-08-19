beb09e59cd5913720e1b37a9d0ee9fcaf071b973
sl4b <57822954+sl4b@users.noreply.github.com>
2022-08-19 15:21:18 -0400

    Simplify TourEditor

--- lib/views/editor/tour.dart
+++ lib/views/editor/tour.dart
@@ -31,43 +31,35 @@ class _TourEditorState extends State<TourEditor> {
 
   @override
   Widget build(BuildContext context) {
-    Widget inner;
-    if (_tour == null) {
-      inner = const Text("Error: tour not found!");
-    } else {
-      var tour = _tour!;
-      inner = Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: TextEditingController(text: tour.name),
-              onChanged: (value) {
-                tour.name = value;
-                _updateSaveTimer();
-              },
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                isDense: true,
-                labelText: "Title",
-              ),
-            ),
-          ),
-          const Divider(
-            height: 1.0,
-            thickness: 1.0,
-          ),
-        ],
-      );
-    }
+    if (!_tourLoaded) return const Center(child: CircularProgressIndicator());
+    if (_tour == null) return const Text("Error: tour not found!");
 
-    return Stack(
+    const inputDecoration = InputDecoration(
+      border: OutlineInputBorder(),
+      floatingLabelBehavior: FloatingLabelBehavior.always,
+      floatingLabelAlignment: FloatingLabelAlignment.start,
+      isDense: true,
+      labelText: "Title",
+    );
+
+    var tour = _tour!;
+
+    return Column(
       children: [
-        if (!_tourLoaded) const Center(child: CircularProgressIndicator()),
-        AnimatedOpacity(
-          opacity: _tourLoaded ? 1.0 : 0.0,
-          duration: const Duration(milliseconds: 100),
-          child: inner,
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            controller: TextEditingController(text: tour.name),
+            onChanged: (value) {
+              tour.name = value;
+              _updateSaveTimer();
+            },
+            decoration: inputDecoration,
+          ),
+        ),
+        const Divider(
+          height: 1.0,
+          thickness: 1.0,
         ),
       ],
     );
