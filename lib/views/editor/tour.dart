b156402c21b00618a818d024591dc9c5440a9d1d
sl4b <57822954+sl4b@users.noreply.github.com>
2022-08-19 16:08:33 -0400

    Add description and map to TourEditor

--- lib/views/editor/tour.dart
+++ lib/views/editor/tour.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_map/plugin_api.dart';
+import 'package:latlong2/latlong.dart';
 
 import '../../db/db.dart';
 
@@ -31,37 +33,88 @@ class _TourEditorState extends State<TourEditor> {
 
   @override
   Widget build(BuildContext context) {
-    if (!_tourLoaded) return const Center(child: CircularProgressIndicator());
-    if (_tour == null) return const Text("Error: tour not found!");
+    if (_tourLoaded && _tour == null) {
+      // TODO: show error popup
+      throw Exception("Error: loaded tour is null");
+    }
 
     const inputDecoration = InputDecoration(
       border: OutlineInputBorder(),
       floatingLabelBehavior: FloatingLabelBehavior.always,
       floatingLabelAlignment: FloatingLabelAlignment.start,
       isDense: true,
-      labelText: "Title",
     );
 
-    var tour = _tour!;
+    var inputsEnabled = _tour != null && _tourLoaded;
 
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextField(
-            controller: TextEditingController(text: tour.name),
-            onChanged: (value) {
-              tour.name = value;
-              _updateSaveTimer();
-            },
-            decoration: inputDecoration,
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          TextField(
+            enabled: inputsEnabled,
+            controller:
+                inputsEnabled ? TextEditingController(text: _tour!.name) : null,
+            onChanged: inputsEnabled
+                ? (value) {
+                    _tour!.name = value;
+                    _updateSaveTimer();
+                  }
+                : null,
+            decoration: inputDecoration.copyWith(labelText: "Title"),
           ),
-        ),
-        const Divider(
-          height: 1.0,
-          thickness: 1.0,
-        ),
-      ],
+          const SizedBox(height: 16.0),
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: TextField(
+                    minLines: 8,
+                    maxLines: 8,
+                    enabled: inputsEnabled,
+                    controller: inputsEnabled
+                        ? TextEditingController(text: _tour!.desc)
+                        : null,
+                    onChanged: inputsEnabled
+                        ? (value) {
+                            _tour!.desc = value;
+                            _updateSaveTimer();
+                          }
+                        : null,
+                    decoration:
+                        inputDecoration.copyWith(labelText: "Description"),
+                  ),
+                ),
+                const SizedBox(width: 16.0),
+                Expanded(
+                  flex: 2,
+                  child: FlutterMap(
+                    options: MapOptions(
+                      center: LatLng(37.09024, -95.712891),
+                      zoom: 4,
+                      maxZoom: 18,
+                    ),
+                    layers: [
+                      TileLayerOptions(
+                        urlTemplate:
+                            "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                        userAgentPackageName: 'org.evresi.builder',
+                      ),
+                    ],
+                    nonRotatedChildren: [
+                      AttributionWidget.defaultWidget(
+                        source: 'OpenStreetMap contributors',
+                        onSourceTapped: null,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
